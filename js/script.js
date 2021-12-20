7449c06332ec309f66c0ba91c9024f971f3a2d47
Ilya Moroz <mermermer@inbox.ru>
2021-12-20 16:07:54 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* let borderWidth = document.querySelector('.border-width')
+console.log(borderWidth)
+borderWidnth() */
+
+
+let fontPlus = document.querySelector('.font-plus')
+console.log(fontPlus)
+fontPlus.addEventListener('click', () =>
+    document.body.
+)
